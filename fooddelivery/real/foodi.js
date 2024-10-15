4c47c14d117fc3c5c967f40f1f3ddb94eceff548
hariharan-spec2 <147948243+SJ-UNI@users.noreply.github.com>
2024-10-15 10:01:05 +0530

    Update foodi.js

--- fooddelivery/real/foodi.js
+++ fooddelivery/real/foodi.js
@@ -279,7 +279,7 @@ const foodItem= [
     category : 'chinese',
     rating : 4.3,
     price: 125,
-    img: 'images/chinese/ChickenManchurian.jpg',
+    img: 'https://raw.githubusercontent.com/SJ-UNI/full-stack/refs/heads/main/fooddelivery/real/images/chinese/ChickenManchurian.jpg',
     quantity: 1
 },
 {
